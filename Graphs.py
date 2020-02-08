4b562dfebc0781acc749d0b051952312e5936900
AndreasAAR <Andreas.ahrlund@gmail.com>
2020-02-08 20:28:36 +0100

    Improved graph for rotation

--- Graphs.py
+++ Graphs.py
@@ -13,14 +13,18 @@ def barPlotPercentage(vals,names):
 
     #range of x axis
     x = (range(len(bars)))
-    new_x = [4 * i for i in x]
+    new_x = [6 * i for i in x]
 
     y_pos = np.arange(len(x))
     # Create bars
     plt.bar(new_x, height,width=2,align='center')
     # Create names on the x-axis   #Rotate names
-    plt.xticks(new_x, bars ,rotation=90,fontsize=5)
+    plt.xticks(new_x, bars ,rotation=90,fontsize=6.5)
+    plt.yticks(rotation = 90)
 
     # Show graphic
-    plt.figure(figsize=(30, 4))
+    #plt.figure(figsize=(30, 4))
+    plt.tight_layout()
+    fig1 = plt.gcf()
+    fig1.savefig('Class Division.png', bbox_extra_artists=(plt.xticks()), bbox='tight')
     plt.show()
